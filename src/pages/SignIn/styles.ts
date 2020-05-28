567d17e268d72bba08d075d1f34209c2515779d4
diogorodrigues <diogo.rodrigues.diogo@gmail.com>
2020-05-28 09:52:16 -0300

    Adding fonts and texts

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'Roboto-Regular';
+`;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Container } from './styles';
+import * as S from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return <S.Container />;
 };
 
 export default SignUp;
